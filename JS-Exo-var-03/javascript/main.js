39e9dd99483863d5c22af64f023b91700d7bf15b
Ab-Amin <abourrabiaamin@gmail.com>
2024-01-21 02:14:30 +0100

    #3 - var-03 ✓

--- JS-Exo-var-03/javascript/main.js
+++ JS-Exo-var-03/javascript/main.js
@@ -1,15 +1,20 @@
+// var-03 | Concaténer des Chaînes de Caractères
+// Déclarez 3 variables contenant des chaînes de caractères.
+// Concaténez-les en une seule chaîne.
+// Injectez le résultat dans le DOM.
 
+let Game1 = "Elden Ring"
+let Game2 = "Baldur's Gate 3"
+let Game3 = "Final Fantasy XIV"
 
+// let Games = Game1 + ", " + Game2 + " and " + Game3
+let Games = `${Game1}, ${Game2} and ${Game3}`
 
+console.log(Games)
 
+let div = document.querySelector('div')
 
-
-
-
-
-
-
-
+div.innerHTML += `my favorite games are ${Games}`
 
 
 

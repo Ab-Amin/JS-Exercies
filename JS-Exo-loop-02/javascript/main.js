1a10dce7b0a66ab5cc0dc7ba22900d377228e92d
Ab-Amin <abourrabiaamin@gmail.com>
2024-01-21 01:42:58 +0100

    #2 - var-01 ✓

--- JS-Exo-loop-02/javascript/main.js
+++ JS-Exo-loop-02/javascript/main.js
@@ -1,3 +1,12 @@
+// var-02 | Incrémenter et décrémenter une variable numérique
+// Déclarez une variable numérique.
+// Incrémentez cette variable grâce à un bouton (<button type=button>Augmenter</button>)
+// Décrémentez cette variable grâce à un autre bouton
+// Affichez la valeur de la variable dès le chargement de la page dans un élément HTML
+// Remplacez la valeur de la variable après chaque incrémentation ou décrémentation (et changez-la aussi dans l’HTML).
+
+
+let div = document.querySelector('div')
 
 
 

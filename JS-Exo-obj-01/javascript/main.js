9838d1471d62525b69dbfb8123aa7e27cac9aada
Ab-Amin <abourrabiaamin@gmail.com>
2024-01-22 02:47:15 +0100

    #6 - obj-01 ✓

--- JS-Exo-obj-01/javascript/main.js
+++ JS-Exo-obj-01/javascript/main.js
@@ -1,8 +1,14 @@
+// obj-01 | Créer et afficher un objet simple
+// Déclarez un objet littéral avec quelques propriétés (par exemple, nom, prenom, age).
+// Affichez les valeurs de ces propriétés dans le DOM (Nom & prénom dans un <h2> et l’âge en indice)
 
+let me = { name : 'Amin', surname : 'Abourrabia', age : 23 }
 
+document.querySelector('body').innerHTML += 
+`
+<h2 title="${me.age} years old">${me.name} ${me.surname}</h2>
 
-
-
+`
 
 
 

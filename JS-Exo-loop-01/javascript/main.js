6c7ce99e35a30cb70ac493b860ca96b7de045241
Ab-Amin <abourrabiaamin@gmail.com>
2024-01-22 03:15:01 +0100

    #8 - loop-01 ✓

--- JS-Exo-loop-01/javascript/main.js
+++ JS-Exo-loop-01/javascript/main.js
@@ -1,7 +1,19 @@
+// loop-01 
+// Créez un tableau de nombres. (35, 22, 153, 5, 67, 256599)
+// Utilisez une boucle for pour parcourir le tableau et ajoutez chaque élément à une variable somme.
+//  Affichez la somme à l'utilisateur.
 
+let numbers = [35, 22, 153, 5, 67, 256599]
 
-
-
+let sum = 0
+for (let i = 0; i < numbers.length; i++) {
+    
+  sum += numbers[i]
+    
+  console.log(sum);
+  
+  document.querySelector('div').innerHTML = sum
+}
 
 
 
